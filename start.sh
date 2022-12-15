1449b4622e9c9ae2dc7049b4e4673a4984ebb309
juouyang <juouyang@gmail.com>
2022-12-15 15:51:27 +0800

    Set sha256 of image in start script

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ docker run -d --name theia \
     -v $(pwd)/demo/project:/home/project \
     -v $(pwd)/demo/theia-settings:/home/theia/.theia \
     -v $(pwd)/demo/ssh-config:/home/theia/.ssh \
-    juouyang/theia-ansible
+    juouyang/theia-ansible@sha256:24a020878b100719ae81d84ad17cd67902a473a13539570fb84d01f2e86e37c7
