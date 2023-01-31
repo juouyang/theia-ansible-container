4ee89b72196a81333e121bb5f4629949a979073e
juouyang <juouyang@gmail.com>
2023-01-31 14:05:44 +0800

    Merge from main branch

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
+mkdir -p $(pwd)/demo/project
+mkdir -p $(pwd)/demo/theia-settings
+mkdir -p $(pwd)/demo/ssh-config
+
 docker run -d --name theia \
     -p 3000:3000 \
     -v $(pwd)/demo/project:/home/project \
     -v $(pwd)/demo/theia-settings:/home/theia/.theia \
     -v $(pwd)/demo/ssh-config:/home/theia/.ssh \
-    juouyang/theia-ansible
+    juouyang/theia-ansible@sha256:24a020878b100719ae81d84ad17cd67902a473a13539570fb84d01f2e86e37c7
